5aa56bfb98bcc4cfbf10c737dc0bd3f56347c7a1
jon White <jewhite3@memphis.edu>
2016-08-10 10:43:38 -0500

    intialized

---
+++ src/day3/day3.java
@@ -0,0 +1,28 @@
+package day3;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class day3 {
+
+	public static void main(String[] args) throws Exception {
+		File file = new File("text.txt");
+		BufferedReader read = new BufferedReader(new FileReader(file));
+		BufferedWriter write = new BufferedWriter(new FileWriter(file));
+		while(true){
+			write.write(read.readLine());
+			
+			"s".length()
+			"e".
+			
+			
+			
+			
+		}
+		}
+	}
+
+}
