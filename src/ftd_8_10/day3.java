7c3c54d079224f45649b1c9d48cd72292aa655b1
jon White <jewhite3@memphis.edu>
2016-08-10 13:09:07 -0500

    implemented File Chooser with nullpointer exception handling. future implementaions should use an action listener for cancel option.

--- src/ftd_8_10/day3.java
+++ src/ftd_8_10/day3.java
@@ -8,16 +8,28 @@ import java.util.Scanner;
 import javax.swing.JFileChooser;
 
 public class day3 {
+	static File file;
 	final static JFileChooser fc = new JFileChooser();
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception  {
 		fc.showOpenDialog(null);
-		File file = fc.getSelectedFile();	
+	try{
+		file = fc.getSelectedFile();
+	}
+	catch(NullPointerException e){
+		file= null;
+		
+	}
+	System.out.println("here");	
+	if(!(file==null)){
+	
 		if(!file.exists()){
+			
 			file.createNewFile();
 			Student stu = new Student("stu","Tennesse","Memphis");
 			BufferedWriter write = new BufferedWriter(new FileWriter(file));
 			write.write(stu.toString());
 			write.close();
+			
 		}
 		else{
 			Scanner sc= new Scanner(file);
@@ -26,7 +38,7 @@ public class day3 {
 				System.out.println(stu.toString());
 			}
 			sc.close();
-			
+		}
 			
 		}
 		
---
+++ src/ftd_8_10/fileTest.java
@@ -0,0 +1,12 @@
+package ftd_8_10;
+
+import java.io.File;
+
+public class fileTest {
+
+	public static void main(String[] args)throws Exception {
+		File file= new File("C:\\temp\\myFile");
+		//file.createNewFile();
+	}
+
+}
