3069a46e89d6fd90478a7dce876b8d73c7a165fa
jon White <jewhite3@memphis.edu>
2016-08-10 11:33:07 -0500

    complteted assignment Student File

---
+++ src/ftd_8_10/Student.java
@@ -0,0 +1,35 @@
+package ftd_8_10;
+
+public class Student {
+private String name;
+private String state;
+private String city;
+	public Student(String name, String state, String city) {
+		this.name=name;
+		this.state= state;
+		this.city = city;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String toString(){
+		
+		return (""+name+"\n"+state+"\n"+city);
+	}
+
+}
--- src/ftd_8_10/day3.java
+++ src/ftd_8_10/day3.java
@@ -1,26 +1,35 @@
 package ftd_8_10;
-
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
+
 import java.io.FileWriter;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 public class day3 {
-
+	final static JFileChooser fc = new JFileChooser();
 	public static void main(String[] args) throws Exception {
-		File file = new File("text.txt");
-		BufferedReader read = new BufferedReader(new FileReader(file));
-		BufferedWriter write = new BufferedWriter(new FileWriter(file));
-		while(true){
-			write.write(read.readLine());
-			
-
-			
-			
+		fc.showOpenDialog(null);
+		File file = fc.getSelectedFile();	
+		if(!file.exists()){
+			file.createNewFile();
+			Student stu = new Student("stu","Tennesse","Memphis");
+			BufferedWriter write = new BufferedWriter(new FileWriter(file));
+			write.write(stu.toString());
+			write.close();
+		}
+		else{
+			Scanner sc= new Scanner(file);
+			while(sc.hasNext()){
+				Student stu =new Student(sc.next(),sc.next(),sc.next());
+				System.out.println(stu.toString());
+			}
+			sc.close();
 			
 			
 		}
+		
 		}
 	}
 
